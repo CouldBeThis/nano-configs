1eb573532ffaaac55937453482ee81a2baeecb79
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-03-10 08:46:46 -0500

    script to install this repo into ~/.config/nano

--- tools/install.sh
+++ tools/install.sh
@@ -4,7 +4,7 @@ InstallDir=$HOME/.config/nano
 
 echo $InstallDir
 
-mv InstallDir/nano InstallDir/nano-old
+mv $InstallDir/nano $InstallDir/nano-old
 
 git clone --recursive git@github.com:CouldBeThis/nano-configs.git $InstallDir
 
